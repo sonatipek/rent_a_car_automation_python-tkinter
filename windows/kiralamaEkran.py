2f50e75b5b39e6a2587bcc88641f7684a7f561ee
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-01-06 03:21:05 +0300

    kiralamaEkrani arayüz kodlandı

--- windows/kiralamaEkran.py
+++ windows/kiralamaEkran.py
@@ -18,18 +18,51 @@ def kiralamaEkrani():
         background="white",
         font="Arial 11",
         foreground="#0A2647",
-        width=13).place(x=10, y=30)
+        width=20).place(x=10, y=30)
 
     # Araç Seçimi
     araclar_var = StringVar()
     araclar=dbIslemleri.getCars()
-    Label(kiralamaEkranim, text="Araçlar:", font="Arial 10").place(x=10, y=110)
+    Label(kiralamaEkranim, text="Araçlar:", font="Arial 10").place(x=10, y=60)
     ttk.Combobox(kiralamaEkranim, textvariable=araclar_var, values=araclar,
         state="readonly",
         background="white",
         font="Arial 11",
         foreground="#0A2647",
-        width=13).place(x=10, y=130)
+        width=20).place(x=10, y=80)
+
+    # Kiralanacak Gün Seçimi
+    gun_var = IntVar()
+    Label(kiralamaEkranim, text="Kiralanacak Gün Sayısı:", font="Arial 10").place(x=10, y=110)
+    Entry(kiralamaEkranim, textvariable=gun_var,
+        bg="white",
+        font="Arial 11",
+        fg="#0A2647",
+        relief="flat",
+        width=24
+        ).place(x=10, y=130)
+
+    # Yolculuk Seçimi
+    yolculuk_var = StringVar()
+    Label(kiralamaEkranim, text="Kiralama Hedefi Neresi:", font="Arial 10").place(x=10, y=160)
+    Entry(kiralamaEkranim, textvariable=yolculuk_var,
+        bg="white",
+        font="Arial 11",
+        fg="#0A2647",
+        relief="flat",
+        width=24
+        ).place(x=10, y=180)
+
+        
+    # Müşteri Kaydet Button
+    Button(kiralamaEkranim, text="Kiralama Oluştur", command=None, 
+        bg="#0A2647", 
+        border="0",
+        fg="white",
+        height=2, 
+        width=20,
+        font="Arial 12 bold",
+        cursor="mouse").place(x=10, y=250)
     
     kiralamaEkranim.mainloop()
 
